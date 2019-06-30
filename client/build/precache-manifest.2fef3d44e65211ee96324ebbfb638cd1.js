b05a0494982bdb0f000b21686434451a80d84c0e
WKolaj <WKolaj@gmail.com>
2019-06-30 23:49:25 +0200

    Added simple react front-end

---
+++ client/build/precache-manifest.2fef3d44e65211ee96324ebbfb638cd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcbea6c634741ae1620ac4e724125e66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1ccdbcce5ab337b845a",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "91b95ae205a8fb2441a9",
+    "url": "/static/js/2.6a4e8b72.chunk.js"
+  },
+  {
+    "revision": "e1ccdbcce5ab337b845a",
+    "url": "/static/js/main.2cc93542.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
