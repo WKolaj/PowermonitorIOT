31b7c04ed0c57f1950912780f4d27f2502762a6b
WKolaj <WKolaj@gmail.com>
2019-07-01 21:21:08 +0200

    Fixed simple react front-end issues

---
+++ client/build/precache-manifest.8256d76d707e6d9c8208cc267f850378.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f63f319b423cb35c36af5807babb44f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a53103e36720ee1adcc",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "89d9414129fa38f8fcf6",
+    "url": "/static/js/2.e64b4ac7.chunk.js"
+  },
+  {
+    "revision": "4a53103e36720ee1adcc",
+    "url": "/static/js/main.bf88e0a1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2fef3d44e65211ee96324ebbfb638cd1.js"
+  "/precache-manifest.8256d76d707e6d9c8208cc267f850378.js"
 );
 
 self.addEventListener('message', (event) => {
